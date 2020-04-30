59527148e23ad775a2b37a02c5587d2bd64e6793
Mafinar Khan <mafinar@gmail.com>
2020-04-30 01:02:53 -0400

    Add os_mon

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Fleetr.MixProject do
   def application do
     [
       mod: {Fleetr.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
