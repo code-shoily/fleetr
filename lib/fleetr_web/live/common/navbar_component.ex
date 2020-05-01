4ae021abc1a34af429a17089e012394a9222397d
Mafinar Khan <mafinar@gmail.com>
2020-05-01 01:47:41 -0400

    Add top-navbar to root view

---
+++ lib/fleetr_web/live/common/navbar_component.ex
@@ -0,0 +1,63 @@
+defmodule FleetrWeb.Common.NavbarComponent do
+  alias Fleetr.Auth
+
+  use FleetrWeb, :live_component
+
+  def update(assigns, socket) do
+    user = Auth.user_by_id(assigns.user_id)
+
+    {:ok, assign(socket, user: user)}
+  end
+
+  def handle_event("user-update", _, socket) do
+    IO.inspect("Profile will be updated for #{user_id(socket)}")
+    {:noreply, socket}
+  end
+
+  def handle_event("user-update-password", _, socket) do
+    IO.inspect("Password will be changed for #{user_id(socket)}")
+    {:noreply, socket}
+  end
+
+  defp user_id(socket), do: socket.assigns.user["user_id"]
+
+  def render(assigns) do
+    ~L"""
+    <nav class="navbar is-primary">
+      <div class="navbar-brand">
+        <div class="navbar-item">
+          <span class="title is-3 has-text-white">
+          <span class="has-text-weight-light">Fleetr</span>
+          </span>
+        </div>
+        <div class="navbar-burger burger" data-target="nav-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div id="nav-menu" class="navbar-menu">
+      <div class="navbar-end">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-item has-text-white navbar-link">
+            <%= @user.email %>
+          </a>
+          <div class="navbar-dropdown is-right">
+            <a class="navbar-item" href="#" phx-click="user-update" phx-target="<%= @myself %>">
+              Update Profile
+            </a>
+            <a class="navbar-item" href="#" phx-click="user-update-password" phx-target="<%= @myself %>">
+              Change Password
+            </a>
+            <hr class="navbar-divider">
+            <a class="navbar-item has-text-danger" href="/sign-out">
+              Sign-out
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </nav>
+    """
+  end
+end
--- lib/fleetr_web/live/home_live.ex
+++ lib/fleetr_web/live/home_live.ex
@@ -1,10 +1,36 @@
 defmodule FleetrWeb.HomeLive do
   @moduledoc false
 
+  alias Fleetr.Cache, as: TokenCache
+  alias FleetrWeb.Auth
+
   use FleetrWeb, :live_view
 
-  @impl true
-  def mount(_params, _session, socket) do
+  def mount(_params, %{"token" => nil}, socket) do
+    send(self(), :unauthorized)
     {:ok, socket}
+  end
+
+  def mount(_params, %{"token" => token}, socket) do
+    case TokenCache.get(token) do
+      nil ->
+        send(self(), :unauthorized)
+        {:ok, socket}
+      user_id -> {:ok, assign(socket, user_id: user_id)}
+    end
+  end
+
+  def mount(_params, _session, socket) do
+    send(self(), :unauthorized)
+    {:ok, socket}
+  end
+
+  def handle_info(:unauthorized, socket) do
+    socket =
+      socket
+      |> put_flash(:error, "You are not authenticated. Please sign in.")
+      |> push_redirect(to: Routes.live_path(socket, Auth.LoginLive))
+
+    {:noreply, socket}
   end
 end
